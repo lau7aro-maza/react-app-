55f608d291877467ae4aaa624d1a5826baedcf67
Lautaro Maza <lautarodamian33@gmail.com>
2022-03-29 22:54:56 -0300

    creacion de navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/NavBar";
 
-function App() {
-
-let mystring = 'hola mundo';
-
+function App(){
   return (
-    <div className="App">
-      {mystring}
+    <div>
+      <Navbar />
     </div>
   );
 }
