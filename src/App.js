cd9fc1be4d45b74896edb932b66de255223df2ab
Lautaro Maza <lautarodamian33@gmail.com>
2022-04-05 17:06:12 -0300

    actualizacion de navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+<<<<<<< HEAD
 import React from "react";
 import Navbar from "./components/Navbar";
 
 
+=======
+import logo from './logo.svg';
+import './App.css';
 
-function App(){
+function App() {
+
+let mystring = 'hola mundo';
+>>>>>>> parent of 55f608d (creacion de navbar)
+
   return (
+<<<<<<< HEAD
     <>
     <Navbar/>
     </>
+=======
+    <div className="App">
+      {mystring}
+    </div>
+>>>>>>> parent of 55f608d (creacion de navbar)
   );
 }
 
