3afa4d1117ee116f8b40abd4764a19bc54f623e8
Lautaro Maza <lautarodamian33@gmail.com>
2022-04-05 17:18:21 -0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from "react";
 import Navbar from "./components/Navbar";
 
 
 =======
+=======
+>>>>>>> parent of 55f608d (creacion de navbar)
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
 let mystring = 'hola mundo';
+<<<<<<< HEAD
 >>>>>>> parent of 55f608d (creacion de navbar)
 
   return (
@@ -18,6 +22,10 @@ let mystring = 'hola mundo';
     <Navbar/>
     </>
 =======
+=======
+
+  return (
+>>>>>>> parent of 55f608d (creacion de navbar)
     <div className="App">
       {mystring}
     </div>
